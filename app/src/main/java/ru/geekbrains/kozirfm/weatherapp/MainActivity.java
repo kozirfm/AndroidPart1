30d6bff34102977d77cf1521303ead3bfe8013a1
Aleksandr Kazirickiy <kozirfm@yandex.ru>
2020-04-15 22:38:03 +0300

    Android Part 1, Home Work, Lesson3, Override methods

--- app/src/main/java/ru/geekbrains/kozirfm/weatherapp/MainActivity.java
+++ app/src/main/java/ru/geekbrains/kozirfm/weatherapp/MainActivity.java
@@ -2,6 +2,7 @@ package ru.geekbrains.kozirfm.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_city_activity);
+        makeToast("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeToast("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy");
+    }
+
+    private void makeToast(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
+
