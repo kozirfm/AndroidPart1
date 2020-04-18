f1a54f9add8f6d8a4918320cd160ea4dad2a739f
Aleksandr Kazirickiy <kozirfm@yandex.ru>
2020-04-18 21:18:04 +0300

    Android Part 1, Home Work, Lesson3, Delete Toast and Log

--- app/src/main/java/ru/geekbrains/kozirfm/weatherapp/MainActivity.java
+++ app/src/main/java/ru/geekbrains/kozirfm/weatherapp/MainActivity.java
@@ -3,13 +3,10 @@ package ru.geekbrains.kozirfm.weatherapp;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LIFE_CYCLE_MESSAGE = "LIFE_CYCLE_MESSAGE";
     private static final String MAIN_CITY = "MAIN_CITY";
     private static final String MAIN_TEMPERATURE = "MAIN_TEMPERATURE";
     private TextView mainCity;
@@ -23,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         mainTemperature = findViewById(R.id.mainTemperature);
         mainCity.setText(R.string.city_moscow);
         mainTemperature.setText(R.string.main_temperature);
-        makeToast("onCreate");
     }
 
     @Override
@@ -40,46 +36,4 @@ public class MainActivity extends AppCompatActivity {
         mainCity.setText(savedInstanceState.getString(MAIN_CITY));
         mainTemperature.setText(savedInstanceState.getString(MAIN_TEMPERATURE));
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        makeToast("onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        makeToast("onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        makeToast("onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        makeToast("onStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        makeToast("onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        makeToast("onDestroy");
-    }
-
-    private void makeToast(String msg){
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-        Log.d(LIFE_CYCLE_MESSAGE, msg);
-    }
 }
-
