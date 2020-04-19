f1daff9798bef20a0b901f1b17323038ba4a085c
Aleksandr Kazirickiy <kozirfm@yandex.ru>
2020-04-19 21:52:42 +0300

    Android Part 1, Home Work, Lesson4

--- app/src/main/java/ru/geekbrains/kozirfm/weatherapp/MainActivity.java
+++ app/src/main/java/ru/geekbrains/kozirfm/weatherapp/MainActivity.java
@@ -1,16 +1,26 @@
 package ru.geekbrains.kozirfm.weatherapp;
 
+import android.content.Intent;
+import android.os.Parcelable;
+
+import org.parceler.Parcels;
+
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Constants {
 
-    private static final String MAIN_CITY = "MAIN_CITY";
-    private static final String MAIN_TEMPERATURE = "MAIN_TEMPERATURE";
+
     private TextView mainCity;
     private TextView mainTemperature;
+    private TextView mainWindPower;
+    private TextView mainPressure;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +28,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mainCity = findViewById(R.id.mainCity);
         mainTemperature = findViewById(R.id.mainTemperature);
+        mainWindPower = findViewById(R.id.mainWindPower);
+        mainPressure = findViewById(R.id.mainPressure);
         mainCity.setText(R.string.city_moscow);
         mainTemperature.setText(R.string.main_temperature);
+        mainWindPower.setText(R.string.main_wind_power);
+        mainPressure.setText(R.string.main_pressure);
+        Button selectCityButton = findViewById(R.id.selectCityButton);
+        selectCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SelectCityActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_SELECTED_CITY_ACTIVITY);
+            }
+        });
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(MAIN_CITY, mainCity.getText().toString());
-        outState.putString(MAIN_TEMPERATURE,mainTemperature.getText().toString());
+        outState.putString(MAIN_TEMPERATURE, mainTemperature.getText().toString());
+        outState.putString(MAIN_WIND_POWER, mainWindPower.getText().toString());
+        outState.putString(MAIN_PRESSURE, mainPressure.getText().toString());
 
     }
 
@@ -35,5 +59,27 @@ public class MainActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
         mainCity.setText(savedInstanceState.getString(MAIN_CITY));
         mainTemperature.setText(savedInstanceState.getString(MAIN_TEMPERATURE));
+        mainWindPower.setText(savedInstanceState.getString(MAIN_WIND_POWER));
+        mainPressure.setText(savedInstanceState.getString(MAIN_PRESSURE));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode != REQUEST_CODE_SELECTED_CITY_ACTIVITY){
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        try {
+            if (resultCode == RESULT_OK) {
+                Parcelable parcelable = data.getParcelableExtra(SELECTED_CITY);
+                WeatherCity weatherCity = Parcels.unwrap(parcelable);
+                mainCity.setText(weatherCity.city);
+                mainTemperature.setText(weatherCity.temperature);
+                mainWindPower.setText(weatherCity.temperature);
+                mainPressure.setText(weatherCity.pressure);
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/ru/geekbrains/kozirfm/weatherapp/SelectCityActivity.java
+++ app/src/main/java/ru/geekbrains/kozirfm/weatherapp/SelectCityActivity.java
@@ -1,14 +1,46 @@
 package ru.geekbrains.kozirfm.weatherapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+
+import org.parceler.Parcels;
+
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class SelectCityActivity extends AppCompatActivity {
+public class SelectCityActivity extends AppCompatActivity implements Constants {
+
+    private RadioGroup cityList;
+    private RadioButton selectedCity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_city);
+        cityList = findViewById(R.id.checkedCity);
+        Button sendSelectedCity = findViewById(R.id.sendSelectedCityButton);
+        sendSelectedCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int id = cityList.getCheckedRadioButtonId();
+                    selectedCity = findViewById(id);
+                    WeatherCity weatherCity = new WeatherCity(selectedCity.getText().toString(), "+25", "755", "10");
+                    Parcelable parcelable = Parcels.wrap(weatherCity);
+                    Intent intentResult = new Intent();
+                    intentResult.putExtra(SELECTED_CITY, parcelable);
+                    setResult(RESULT_OK, intentResult);
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        });
     }
 }
