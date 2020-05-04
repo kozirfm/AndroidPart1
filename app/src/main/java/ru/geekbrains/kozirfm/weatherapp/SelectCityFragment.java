e853e5c4c16f1ae57194fd5b85d567beaa42f5d5
Aleksandr Kazirickiy <kozirfm@yandex.ru>
2020-05-04 13:31:51 +0300

    Android Part 1, Home Work, Lesson7, Add Bottom Navigation and dark theme

--- app/src/main/java/ru/geekbrains/kozirfm/weatherapp/SelectCityFragment.java
+++ app/src/main/java/ru/geekbrains/kozirfm/weatherapp/SelectCityFragment.java
@@ -1,9 +1,11 @@
 package ru.geekbrains.kozirfm.weatherapp;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,7 +34,11 @@ public class SelectCityFragment extends Fragment {
             public void onItemClick(View view, int position) {
                 TextView textView = getActivity().findViewById(R.id.mainCity);
                 textView.setText(((TextView) view).getText());
-                getActivity().getSupportFragmentManager().popBackStack();
+                getActivity().
+                        getSupportFragmentManager().
+                        beginTransaction().
+                        hide(getActivity().getSupportFragmentManager().findFragmentById(R.id.fragmentPart)).
+                        commit();
             }
         });
     }
